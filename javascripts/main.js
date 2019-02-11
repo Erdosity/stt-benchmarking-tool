6329c48dd0d2ecffb4dd21c46a426c359eefc0ce
Erdosity <47506265+Erdosity@users.noreply.github.com>
2019-02-11 10:49:38 -0800

    trigger event to calculate costs for default
    trigger calculate event to update costs based on default values when we first load page

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,6 +11,7 @@ function STTCalculatorCtrl($scope) {
       $scope.desiredAccuracy = "70";
       $scope.callAuthentication = "unimportant";
       $scope.humanLaborPrice = "15";
+      $scope.calculate();
 
       $scope.calculate = function() {
         console.log("inside calculate function");
